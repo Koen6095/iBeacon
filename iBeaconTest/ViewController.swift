23689e824b3511e9ac7331b9ed4ba5e8c924965e
Koen Vestjens <koenvestjens@gmail.com>
2017-11-23 10:11:59 +0100

    Initial commit

---
+++ iBeaconTest/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  iBeaconTest
+//
+//  Created by Koen Vestjens on 13/10/2017.
+//  Copyright © 2017 Koen Vestjens. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    startScanning()
+                }
+            }
+        }
+    }
+    
+    func startScanning() {
+        //let uuid = UUID(uuidString: "E584FBCB-829C-48B2-88CC-F7142B926AEA")! //01022 10
+        let uuid = UUID(uuidString: "E584FBCB-829C-48B2-88CC-F7142B926AEA")! //01022 8
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 10, minor: 1, identifier: "MyBeacon")
+        
+        locationManager.startMonitoring(for: beaconRegion)
+        locationManager.startRangingBeacons(in: beaconRegion)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+        if beacons.count > 0 {
+            updateDistance(beacons[0].proximity)
+        } else {
+            updateDistance(.unknown)
+        }
+    }
+    
+    func updateDistance(_ distance: CLProximity) {
+        UIView.animate(withDuration: 0.8) {
+            switch distance {
+            case .unknown:
+                self.view.backgroundColor = UIColor.gray
+                
+            case .far:
+                self.view.backgroundColor = UIColor.blue
+                
+            case .near:
+                self.view.backgroundColor = UIColor.orange
+                
+            case .immediate:
+                self.view.backgroundColor = UIColor.red
+            }
+        }
+    }
+
+}
+
+//https://www.hackingwithswift.com/example-code/location/how-to-detect-ibeacons
+
